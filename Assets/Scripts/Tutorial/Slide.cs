e1fa711bf567be7f6d5efec1a7d845c788d1b795
Luqmus <rjcnz1007@yandex.ru>
2022-08-04 15:45:22 +0400

    Rework panel hints
    create scriptable objects

--- Assets/Scripts/Tutorial/Slide.cs
+++ Assets/Scripts/Tutorial/Slide.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Slide : MonoBehaviour
+[CreateAssetMenu(menuName = "Create Slide Info", fileName = "Slide", order = 1)]
+public class Slide : ScriptableObject
 {
     [SerializeField] private Sprite _image;
     [SerializeField] private string _hint;
